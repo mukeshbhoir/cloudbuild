138dcb242d50eabe14f5ec2fa57f01a59e7d9cdd
mukeshbhoir <mukeshbhoir92@gmail.com>
2023-12-11 17:38:38 +0530

    fixed syntax

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "project-name" {
-  type    = "corded-shift-399205"
+variable project-name {
+  type    = corded-shift-399205
 }
 
-variable "region" {
-  type    = "asia-south1-b"
-  default = "asia-south1-a"
+variable region {
+  type    = asia-south1-b
+  default = asia-south1-a
 }
