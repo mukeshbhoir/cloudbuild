c18e7dfd04f9978eb01de25d74a63ceb9a08d160
mukeshbhoir <mukeshbhoir92@gmail.com>
2023-12-12 12:41:13 +0530

    3

--- main.tf
+++ main.tf
@@ -15,44 +15,6 @@ resource "google_compute_subnetwork" "mumbai-subnet-1" {
   region        = "asia-south1"
   network       = google_compute_network.terraform_network.id
 }
-  resource "google_compute_subnetwork" "delhi-subnet-1" {
-  name          = "delhi-subnet-1"
-  ip_cidr_range = "200.0.6.0/24"
-  region        = "asia-south2"
-  network       = google_compute_network.terraform_network.id
-}
-resource "google_compute_firewall" "allow-ssh" {
-  name    = "allow-ssh"
-  network       = google_compute_network.terraform_network.id
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-}
-resource "google_compute_firewall" "allow-internal" {
-  name    = "allow-internal"
-  network       = google_compute_network.terraform_network.id
-  allow {
-    protocol = "tcp"
-    ports    = ["1-65535"]
-  }
-  allow {
-  protocol = "udp"
-    ports    = ["1-65535"]
-  }
-   allow {
-  protocol = "icmp"
-  }
-  source_ranges = ["200.0.5.0/24", "200.0.6.0/24" ]
-}
-
-
-resource "google_compute_address" "ephemeral" {
-  name = "ipv4-address"
-  region  = "asia-south1"
-}
-
 
 resource "google_compute_instance" "computevm1" {
   name                      = "test-vm1"
@@ -64,7 +26,6 @@ resource "google_compute_instance" "computevm1" {
     subnetwork = "mumbai-subnet-1"
     network_ip = "200.0.5.15"
     access_config {
-    nat_ip = google_compute_address.ephemeral.address
     }
   }
   boot_disk {
